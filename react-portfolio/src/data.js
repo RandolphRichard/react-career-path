2bff2d76d13bbda54dd8b460f02e57ca807be9e8
Randolph Richard <randolphrichard9@gmail.com>
2022-01-15 14:44:58 -0500

    added some assets,Portfolio files,project,navbar and built a data file

---
+++ react-portfolio/src/data.js
@@ -0,0 +1,115 @@
+import Voltron from './assets/images/voltron.png';
+import Infinity from './assets/images/infinity.png';
+import Todo from './assets/images/task-tracker.png';
+import WineBlog from './assets/images/wine-blogger.png';
+import WeatherScanner from './assets/images/weather-scanner.png';
+import FanTinder from './assets/images/fan-tinder.png';
+import Taskinator from './assets/images/taskinator.png';
+import PizzaHunt from './assets/images/pizza-hunt.png';
+import PwaBudgetTracker from './assets/images/pwa-budget-tracker.png';
+import TechBlog from './assets/images/tech-blog.png';
+import ShopShop from './assets/images/shop-shop.png';
+import GoogleBooks from './assets/images/book-search.png';
+import ZooKeepr from './assets/images/zoo-keepr.png';
+import WeatherDashboard from './assets/images/weather-dashboard.png';
+
+export default [
+    {
+        "id": 1,
+        "name": "FanTinder",
+        "image": FanTinder,
+        "github": "https://github.com/RandolphRichard/cryptobuzz.git",
+        "deploy": "https://cryptobuzz.herokuapp.com/"
+    },
+    {
+        "id": 2,
+        "name": "Weather Scanner",
+        "image": WeatherScanner,
+        "github": "https://github.com/RandolphRichard/Weather-Dashboard.git",
+        "deploy": "https://randolphrichard.github.io/Weather-Dashboard/"
+    },
+    {
+        "id": 3,
+        "name": "Movie Rentals",
+        "image": Infinity,
+        "github": "https://github.com/RandolphRichard/cryptobuzz.git",
+        "deploy": "https://cryptobuzz.herokuapp.com/"
+    },
+    {
+        "id": 4,
+        "name": "Wine Blog",
+        "image": WineBlog,
+        "github": "https://github.com/RandolphRichard/cryptobuzz.git",
+        "deploy": "https://cryptobuzz.herokuapp.com/"
+    },
+    {
+        "id": 5,
+        "name": "Voltron",
+        "image": Voltron,
+        "github": "https://github.com/RandolphRichard/quiz.git",
+        "deploy": "https://randolphrichard.github.io/quiz/"
+    },
+    {
+        "id": 6,
+        "name": "Shop Shop",
+        "image": ShopShop,
+        "github": "https://github.com/RandolphRichard/food-festival.git",
+        "deploy": "https://randolphrichard.github.io/food-festival/"
+    },
+    {
+        "id": 7,
+        "name": "Taskinator",
+        "image": Taskinator,
+        "github": "https://github.com/RandolphRichard/new-taskinator.git",
+        "deploy": "https://randolphrichard.github.io/new-taskinator/"
+    },
+    {
+        "id": 8,
+        "name": "Pizza Hunt",
+        "image": PizzaHunt,
+        "github": "https://github.com/RandolphRichard/pizza-hunt.git",
+        "deploy": "https://randolphrichard.github.io/pizza-hunt/"
+    },
+    {
+        "id": 9,
+        "name": "PWA Budget Tracker",
+        "image": PwaBudgetTracker,
+        "github": "https://github.com/RandolphRichard/finance-trend.git",
+        "deploy": "https://finances-trends.herokuapp.com/"
+    },
+    {
+        "id": 10,
+        "name": "Tech Blog",
+        "image": TechBlog,
+        "github": "https://github.com/RandolphRichard/DTech-Squad.git",
+        "deploy": "https://tech-blog-posts.herokuapp.com/"
+    },
+    {
+        "id": 11,
+        "name": "Task Tracker",
+        "image": Todo,
+        "github": "https://github.com/RandolphRichard/work-day-planner.git",
+        "deploy": "https://randolphrichard.github.io/work-day-planner/"
+    },
+    {
+        "id": 12,
+        "name": "Book Search Engine",
+        "image": GoogleBooks,
+        "github": "https://github.com/RandolphRichard/marketing-startegy.git",
+        "deploy": "https://randolphrichard.github.io/marketing-startegy/"
+    },
+    {
+        "id": 13,
+        "name": "Zoo Keepr",
+        "image": ZooKeepr,
+        "github": "https://github.com/RandolphRichard/zookeepr.git",
+        "deploy": "https://nameless-chamber-93311.herokuapp.com/"
+    },
+    {
+        "id": 14,
+        "name": "Weather Dashboard",
+        "image": WeatherDashboard,
+        "github": "https://github.com/RandolphRichard/Weather-Dashboard.git",
+        "deploy": "https://randolphrichard.github.io/Weather-Dashboard/"
+    }
+]
