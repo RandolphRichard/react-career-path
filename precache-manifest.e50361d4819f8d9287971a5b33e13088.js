bba8891d91bc8a8318feeb9c75a3eb8860a4757c
Randolph Richard <randolphrichard9@gmail.com>
2022-01-15 21:32:42 -0500

    Updates

---
+++ precache-manifest.e50361d4819f8d9287971a5b33e13088.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c35c5e750f5170f40754ce535ff84b4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0e72585b944f87a02067",
+    "url": "/react-portfolio/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "2eed60ab6c995d83f984",
+    "url": "/react-portfolio/static/css/main.080e75f5.chunk.css"
+  },
+  {
+    "revision": "0e72585b944f87a02067",
+    "url": "/react-portfolio/static/js/2.d671d4c9.chunk.js"
+  },
+  {
+    "revision": "f777c2cd8d43a44e423cc24a2b4ae71a",
+    "url": "/react-portfolio/static/js/2.d671d4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded75092cb068c4b7dfa",
+    "url": "/react-portfolio/static/js/3.75daec87.chunk.js"
+  },
+  {
+    "revision": "2eed60ab6c995d83f984",
+    "url": "/react-portfolio/static/js/main.db550af5.chunk.js"
+  },
+  {
+    "revision": "55a01518d185c89fc1fb",
+    "url": "/react-portfolio/static/js/runtime-main.9212cc19.js"
+  },
+  {
+    "revision": "c00693dc2d7892e42d8c37efc2b15f49",
+    "url": "/react-portfolio/static/media/Crypto.c00693dc.png"
+  },
+  {
+    "revision": "9dabbe88ddadaeab0b6cbc0efb0ae45c",
+    "url": "/react-portfolio/static/media/Finance-Trend.9dabbe88.png"
+  },
+  {
+    "revision": "0eea29297d150bcbf858c90e0ee58532",
+    "url": "/react-portfolio/static/media/Notes.0eea2929.png"
+  },
+  {
+    "revision": "24777aa15c74e7746ec7d5431bce3357",
+    "url": "/react-portfolio/static/media/Quiz.24777aa1.png"
+  },
+  {
+    "revision": "9e7423577352292c101b2da69e869cf0",
+    "url": "/react-portfolio/static/media/Web.9e742357.png"
+  },
+  {
+    "revision": "f46aa65e1b82be978cf7cdb76d66c5fc",
+    "url": "/react-portfolio/static/media/abstract.f46aa65e.jpg"
+  },
+  {
+    "revision": "56c6566b28d3d1abd1b623a3c3c07294",
+    "url": "/react-portfolio/static/media/avatar.56c6566b.png"
+  },
+  {
+    "revision": "c840746985ed3dc0cf15ccdcd108d7f3",
+    "url": "/react-portfolio/static/media/berkeley.c8407469.png"
+  },
+  {
+    "revision": "56a64aafdfcf9f6e020a33efb7326dc5",
+    "url": "/react-portfolio/static/media/bootstrap-logo.56a64aaf.png"
+  },
+  {
+    "revision": "ade3a8cd60ae631e940c9f7f225a0815",
+    "url": "/react-portfolio/static/media/carleton.ade3a8cd.png"
+  },
+  {
+    "revision": "b032de9850e2377ca259d22ef9c2527d",
+    "url": "/react-portfolio/static/media/handlebars-logo.b032de98.png"
+  },
+  {
+    "revision": "fce9e8338e5e473b6a86eb8d9b922634",
+    "url": "/react-portfolio/static/media/jquery-logo.fce9e833.png"
+  },
+  {
+    "revision": "0a84db994aa73a48604aa2a5181352f4",
+    "url": "/react-portfolio/static/media/logo.0a84db99.png"
+  },
+  {
+    "revision": "0aea9cb9d494b02ca4dce0e535e3259f",
+    "url": "/react-portfolio/static/media/mern-logo.0aea9cb9.png"
+  },
+  {
+    "revision": "6ac53c3e76439367e757619c306e903e",
+    "url": "/react-portfolio/static/media/mongodb-logo.6ac53c3e.png"
+  },
+  {
+    "revision": "1fe7fae79463f7e20e620e6c92de7d1e",
+    "url": "/react-portfolio/static/media/pittsburgh.1fe7fae7.png"
+  },
+  {
+    "revision": "4e20bc9a5bb1c68ad48ed6a560c6e894",
+    "url": "/react-portfolio/static/media/pizza-hunt.4e20bc9a.png"
+  },
+  {
+    "revision": "4f08d066a3066a8e1ac14dfe1cbe29c3",
+    "url": "/react-portfolio/static/media/redux-logo.4f08d066.png"
+  },
+  {
+    "revision": "89188f65c36eba833f3070a0d956114c",
+    "url": "/react-portfolio/static/media/shop-shop.89188f65.png"
+  },
+  {
+    "revision": "e1cf1de3cdc63552b1dc567afa4e13fa",
+    "url": "/react-portfolio/static/media/sql-logo.e1cf1de3.png"
+  },
+  {
+    "revision": "df6fbe7bc91a6d3fb272348c19166f4d",
+    "url": "/react-portfolio/static/media/task-tracker.df6fbe7b.png"
+  },
+  {
+    "revision": "e93b3d540fc726695d2562b2efd30a76",
+    "url": "/react-portfolio/static/media/taskinator.e93b3d54.png"
+  },
+  {
+    "revision": "a8a68ad1fbb1c997f1271bcf443a07e3",
+    "url": "/react-portfolio/static/media/tblogger.a8a68ad1.png"
+  },
+  {
+    "revision": "a3f680d341b0de02e5f22de1f320eef3",
+    "url": "/react-portfolio/static/media/tech-blog.a3f680d3.png"
+  },
+  {
+    "revision": "5b777e7d7da546a6f1b11041ce9a202f",
+    "url": "/react-portfolio/static/media/weather-dashboard.5b777e7d.png"
+  },
+  {
+    "revision": "fdf7279305c700c6f7c29c17d3a6c882",
+    "url": "/react-portfolio/static/media/weather-scanner.fdf72793.png"
+  },
+  {
+    "revision": "fb9cd70bfe9fe735a423b2d91a7366bd",
+    "url": "/react-portfolio/static/media/webpack-logo.fb9cd70b.png"
+  },
+  {
+    "revision": "a8dcd0c4950508894ec2ec7c52a87b3d",
+    "url": "/react-portfolio/static/media/zoo-keepr.a8dcd0c4.png"
+  }
+]);
