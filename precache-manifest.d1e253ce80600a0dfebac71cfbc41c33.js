f4ac58eec2ca3e087145822c71061c5a2c63c982
Randolph Richard <randolphrichard9@gmail.com>
2022-01-20 20:01:11 -0500

    Updates

---
+++ precache-manifest.d1e253ce80600a0dfebac71cfbc41c33.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209c61aa89ac1da48e6cc18f181a1957",
+    "url": "/react-career-path/index.html"
+  },
+  {
+    "revision": "0e72585b944f87a02067",
+    "url": "/react-career-path/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "09f2d1d08608e006ded2",
+    "url": "/react-career-path/static/css/main.bc885cf8.chunk.css"
+  },
+  {
+    "revision": "0e72585b944f87a02067",
+    "url": "/react-career-path/static/js/2.d671d4c9.chunk.js"
+  },
+  {
+    "revision": "f777c2cd8d43a44e423cc24a2b4ae71a",
+    "url": "/react-career-path/static/js/2.d671d4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded75092cb068c4b7dfa",
+    "url": "/react-career-path/static/js/3.75daec87.chunk.js"
+  },
+  {
+    "revision": "09f2d1d08608e006ded2",
+    "url": "/react-career-path/static/js/main.dbc19722.chunk.js"
+  },
+  {
+    "revision": "fa0f58b31bac6cb2a49b",
+    "url": "/react-career-path/static/js/runtime-main.9b22d764.js"
+  },
+  {
+    "revision": "c00693dc2d7892e42d8c37efc2b15f49",
+    "url": "/react-career-path/static/media/Crypto.c00693dc.png"
+  },
+  {
+    "revision": "9dabbe88ddadaeab0b6cbc0efb0ae45c",
+    "url": "/react-career-path/static/media/Finance-Trend.9dabbe88.png"
+  },
+  {
+    "revision": "0eea29297d150bcbf858c90e0ee58532",
+    "url": "/react-career-path/static/media/Notes.0eea2929.png"
+  },
+  {
+    "revision": "24777aa15c74e7746ec7d5431bce3357",
+    "url": "/react-career-path/static/media/Quiz.24777aa1.png"
+  },
+  {
+    "revision": "9e7423577352292c101b2da69e869cf0",
+    "url": "/react-career-path/static/media/Web.9e742357.png"
+  },
+  {
+    "revision": "f46aa65e1b82be978cf7cdb76d66c5fc",
+    "url": "/react-career-path/static/media/abstract.f46aa65e.jpg"
+  },
+  {
+    "revision": "56c6566b28d3d1abd1b623a3c3c07294",
+    "url": "/react-career-path/static/media/avatar.56c6566b.png"
+  },
+  {
+    "revision": "c840746985ed3dc0cf15ccdcd108d7f3",
+    "url": "/react-career-path/static/media/berkeley.c8407469.png"
+  },
+  {
+    "revision": "56a64aafdfcf9f6e020a33efb7326dc5",
+    "url": "/react-career-path/static/media/bootstrap-logo.56a64aaf.png"
+  },
+  {
+    "revision": "ade3a8cd60ae631e940c9f7f225a0815",
+    "url": "/react-career-path/static/media/carleton.ade3a8cd.png"
+  },
+  {
+    "revision": "b032de9850e2377ca259d22ef9c2527d",
+    "url": "/react-career-path/static/media/handlebars-logo.b032de98.png"
+  },
+  {
+    "revision": "fce9e8338e5e473b6a86eb8d9b922634",
+    "url": "/react-career-path/static/media/jquery-logo.fce9e833.png"
+  },
+  {
+    "revision": "0a84db994aa73a48604aa2a5181352f4",
+    "url": "/react-career-path/static/media/logo.0a84db99.png"
+  },
+  {
+    "revision": "0aea9cb9d494b02ca4dce0e535e3259f",
+    "url": "/react-career-path/static/media/mern-logo.0aea9cb9.png"
+  },
+  {
+    "revision": "6ac53c3e76439367e757619c306e903e",
+    "url": "/react-career-path/static/media/mongodb-logo.6ac53c3e.png"
+  },
+  {
+    "revision": "1fe7fae79463f7e20e620e6c92de7d1e",
+    "url": "/react-career-path/static/media/pittsburgh.1fe7fae7.png"
+  },
+  {
+    "revision": "4e20bc9a5bb1c68ad48ed6a560c6e894",
+    "url": "/react-career-path/static/media/pizza-hunt.4e20bc9a.png"
+  },
+  {
+    "revision": "4f08d066a3066a8e1ac14dfe1cbe29c3",
+    "url": "/react-career-path/static/media/redux-logo.4f08d066.png"
+  },
+  {
+    "revision": "89188f65c36eba833f3070a0d956114c",
+    "url": "/react-career-path/static/media/shop-shop.89188f65.png"
+  },
+  {
+    "revision": "e1cf1de3cdc63552b1dc567afa4e13fa",
+    "url": "/react-career-path/static/media/sql-logo.e1cf1de3.png"
+  },
+  {
+    "revision": "df6fbe7bc91a6d3fb272348c19166f4d",
+    "url": "/react-career-path/static/media/task-tracker.df6fbe7b.png"
+  },
+  {
+    "revision": "e93b3d540fc726695d2562b2efd30a76",
+    "url": "/react-career-path/static/media/taskinator.e93b3d54.png"
+  },
+  {
+    "revision": "a8a68ad1fbb1c997f1271bcf443a07e3",
+    "url": "/react-career-path/static/media/tblogger.a8a68ad1.png"
+  },
+  {
+    "revision": "a3f680d341b0de02e5f22de1f320eef3",
+    "url": "/react-career-path/static/media/tech-blog.a3f680d3.png"
+  },
+  {
+    "revision": "5b777e7d7da546a6f1b11041ce9a202f",
+    "url": "/react-career-path/static/media/weather-dashboard.5b777e7d.png"
+  },
+  {
+    "revision": "fdf7279305c700c6f7c29c17d3a6c882",
+    "url": "/react-career-path/static/media/weather-scanner.fdf72793.png"
+  },
+  {
+    "revision": "fb9cd70bfe9fe735a423b2d91a7366bd",
+    "url": "/react-career-path/static/media/webpack-logo.fb9cd70b.png"
+  },
+  {
+    "revision": "a8dcd0c4950508894ec2ec7c52a87b3d",
+    "url": "/react-career-path/static/media/zoo-keepr.a8dcd0c4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.c3ec7c7804c88bea3a0605677ef90a8f.js"
+  "/react-career-path/precache-manifest.d1e253ce80600a0dfebac71cfbc41c33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-career-path/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
